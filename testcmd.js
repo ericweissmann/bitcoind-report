8d581965d5d403166361826cd8838991c376a4ce
Eric Weissmann <eric@weissmann.net>
2018-11-30 12:29:22 +0200

    read file

--- testcmd.js
+++ testcmd.js
@@ -1,8 +1,10 @@
 "use strict"
 
-console.log("hello Mr. eric");
+const fs = require('fs');
 
-// upload to server & test
+console.log("hello Mr. eric");
+console.log(fs.readFileSync('/root/.bitcoin/bitcoin.conf'));
+
 // get config file & display
 // parse config file
 // connect to bitcoind & display balance
