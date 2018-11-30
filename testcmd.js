9dad8dbc385a31906d378638478047b2f81d4b37
Eric Weissmann <eric@weissmann.net>
2018-11-30 17:19:22 +0200

    gbalance

--- testcmd.js
+++ testcmd.js
@@ -2,15 +2,24 @@
 
 const fs = require('fs');
 
-const envfile = require('envfile')
+const envfile = require('envfile');
+const BitcoinRpc = require('bitcoin-rpc-promise');
+
+
+// Get the bitcoin configuration
 const sourcePath = '/root/.bitcoin/bitcoin.conf';
+let bitcoinConfig = envfile.parseFileSync(sourcePath);
+console.log(bitcoinConfig);
 
-// Parse an envfile path
-
-console.log(
-    envfile.parseFileSync(sourcePath)
-)
 console.log("hello Mr. eric");
+
+// Connect to bitcoind
+let btc = new BitcoinRpc(`http://${bitcoinConfig.rpcuser}:${bitcoinConfig.rpcpassword}@localhost:8332`);
+
+// get and print balance
+btc.getbalance().then(result => {
+    console.log(result);
+});
 // console.log(fs.readFileSync('/root/.bitcoin/bitcoin.conf').toString());
 
 // get config file & display
