32e3914ff1c987074a543cc90d41ffdec45afe3d
Eric Weissmann <eric@weissmann.net>
2018-11-30 12:42:28 +0200

    parse

--- testcmd.js
+++ testcmd.js
@@ -2,8 +2,16 @@
 
 const fs = require('fs');
 
+const envfile = require('envfile')
+const sourcePath = '/root/.bitcoin/bitcoin.conf';
+
+// Parse an envfile path
+
+console.log(
+    envfile.parseFileSync(sourcePath)
+)
 console.log("hello Mr. eric");
-console.log(fs.readFileSync('/root/.bitcoin/bitcoin.conf').toString());
+// console.log(fs.readFileSync('/root/.bitcoin/bitcoin.conf').toString());
 
 // get config file & display
 // parse config file
